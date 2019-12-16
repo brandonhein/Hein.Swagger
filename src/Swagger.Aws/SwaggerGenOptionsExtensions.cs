362bcde717acff2c521b6379c3f1d41dfe563012
Hein, Brandon <BrandonHein@quickenloans.com>
2019-12-16 17:36:10 -0500

    aws enablement

---
+++ src/Swagger.Aws/SwaggerGenOptionsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Hein.Swagger.Filters;
+using Hein.Swagger.Security.Keys;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Hein.Swagger.Aws
+{
+    public static class SwaggerGenOptionsExtensions
+    {
+        public static void EnforceApiKey(this SwaggerGenOptions options)
+        {
+            var scheme = new HeaderSecurityScheme("x-api-key", "AWS API Gateway x-api-key");
+            options.DocumentFilter<SwaggerSecurityDocumentFilter>(scheme);
+            options.OperationFilter<SwaggerSecurityOperationalFilter>(scheme);
+        }
+    }
+}
