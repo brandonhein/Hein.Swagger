3ad4010ddd93f5f98cf6a86c3e93ee88eda5c0f8
Hein, Brandon <BrandonHein@quickenloans.com>
2020-01-03 15:50:01 -0500

    Create ApiDescriptionExtensions.cs

---
+++ src/Swagger/ApiDescriptionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Hein.Swagger
+{
+    public static class ApiDescriptionExtensions
+    {
+        public static IEnumerable<object> ActionAttributes(this ApiDescription apiDescription)
+        {
+            return GetAttributes(apiDescription);
+        }
+
+        public static IEnumerable<object> ControllerAttributes(this ApiDescription apiDescription)
+        {
+            return GetAttributes(apiDescription);
+        }
+
+        private static IEnumerable<object> GetAttributes(ApiDescription apiDescription)
+        {
+            if (apiDescription.TryGetMethodInfo(out MethodInfo methodInfo))
+            {
+                return methodInfo.GetCustomAttributes(true)
+                    .Union(methodInfo.DeclaringType.GetCustomAttributes(true));
+            }
+
+            return Enumerable.Empty<object>();
+        }
+    }
+}
