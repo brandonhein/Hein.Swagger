993a1eee85354afa0bc7a9ea8c2b073314e6366d
Brandon Hein <bhein@lssu.edu>
2020-03-13 14:34:26 -0400

    unqiue operation ids for valid swaggers

---
+++ src/Swagger/Filters/SwaggerUniqueOperationIdFilter.cs
@@ -0,0 +1,14 @@
+﻿using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+
+namespace Hein.Swagger.Filters
+{
+    public class SwaggerUniqueOperationIdFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            operation.OperationId = Guid.NewGuid().ToString();
+        }
+    }
+}
